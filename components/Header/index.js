dce9c1966e5e86429d24f7bd4cced2054b32a22e
Trevor Thompson <trevthom18@gmail.com>
2019-08-16 20:44:44 -0400

    I've attempted the first third of the assignment. I've coded it, but it isn't showing up anywhere

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,26 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+function Header() {
+
+    const divCreator = document.createElement('div');
+    const h1Creator = document.createElement('h1');
+    const spanCreator = document.createElement('span');
+    const dateCreator = document.createElement('span');
+
+    divCreator.classList.add('header');
+    spanCreator.classList.add('temp');
+    dateCreator.classList.add('date');
+
+    h1Creator.textContent = 'Lambda Times';
+    spanCreator.textContent = '98';
+    dateCreator.textContent = 'SMARCH 28, 2019';
+
+    const containerOne = document.getElementsByName('.headerContainer');
+
+    containerOne.appendChild(divCreator);
+    containerOne.appendChild(dateCreator);
+    containerOne.appendChild(h1Creator);
+    containerOne.appendChild(spanCreator);
+
+}
