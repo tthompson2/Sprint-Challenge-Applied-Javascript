9ff08fa6ce009af8dcd5be7901fed6fcb6770879
Trevor Thompson <trevthom18@gmail.com>
2019-10-11 21:40:32 -0400

    Getting closer, but still haven't managed to finish the last third of the assignment

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,41 +18,52 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-const createCard = document.createElement('div');
-     const createHeadline = document.createElement('div');
-     const createAuthor = document.createElement('div');
-     const createImgContainer = document.createElement('div');
-     const createImg = document.createElement('img');
-     const newSpan = document.createElement('span');
-     
-     createCard.classList.add('card');
-     createHeadline.classList.add('headline');
-     createAuthor.classList.add('author');
-     createImgContainer.classList.add('img-container');
+const cardContainer = document.querySelector('.cards-container');
 
 axios
      .get('https://lambda-times-backend.herokuapp.com/articles')
 
-     .then(response => {
-         //console.log(Response);
-         /*console.log(Response.data.articles.bootstrap);
+     .then(Response => {
+         console.log(Response.data.articles.bootstrap);
          console.log(Response.data.articles.javascript);
          console.log(Response.data.articles.node);
          console.log(Response.data.articles.technology);
-         console.log(Response.data.articles.jquery);*/
+         console.log(Response.data.articles.jquery);
 
-         createHeadline.textContent = response.data.articles.bootstrap.headline;
-         newSpan.textContent = response.data.articles.bootstrap.authorName;
-         createImg.src = response.data.articles.bootstrap.authorPhoto;
+         function cardCreator() {
+
+         Response.data.articles.bootstrap.forEach(element => {
          
+         const createCard = document.createElement('div');
+         const createHeadline = document.createElement('div');
+         const createAuthor = document.createElement('div');
+         const createImgContainer = document.createElement('div');
+         const createImg = document.createElement('img');
+         const newSpan = document.createElement('span');
+
+         createCard.classList.add('card');
+         createHeadline.classList.add('headline');
+         createAuthor.classList.add('author');
+         createImgContainer.classList.add('img-container');   
+            
+         newSpan.textContent = element.authorName;
+         createImg.src = element.authorPhoto;
+         createHeadline.textContent = element.headline;
+
          createCard.appendChild(createHeadline);
          createCard.appendChild(createAuthor);
          createAuthor.appendChild(createImgContainer);
          createImgContainer.appendChild(createImg);
          createAuthor.appendChild(newSpan);
+
+         return createCard;
+    
+});
+         }
+
+         cardContainer.appendChild(cardCreator());
      })
      .catch(error => {
          console.log(error);
      })
-
      
